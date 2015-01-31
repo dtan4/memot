48dcd0f3e86d353f935f316aadfb27185fe6ddce
Daisuke Fujita <dtanshi45@gmail.com>
2015-02-01 02:49:00 +0900

    Create Client class and remove some dependencies

--- lib/memot.rb
+++ lib/memot.rb
@@ -5,6 +5,7 @@ require "memot/version"
 require "memot/dropbox"
 require "memot/evernote"
 require "memot/markdown"
+require "memot/client"
 
 module Memot
   # Your code goes here...
--- lib/memot/dropbox.rb
+++ lib/memot/dropbox.rb
@@ -1,12 +1,9 @@
 require "dropbox_sdk"
-require "memot/evernote"
-require "memot/markdown"
 
 module Memot
   class DropboxCli
-    def initialize(access_token, evernote, redis, logger)
+    def initialize(access_token, redis, logger)
       @client = DropboxClient.new(access_token)
-      @evernote = evernote
       @redis = redis
       @logger = logger
     end
@@ -15,19 +12,30 @@ module Memot
       latest_revision = get_revision(path)
       updated_revision = latest_revision
 
+      need_update = []
+
       @client.metadata(path)["contents"].each do |cont|
         cont_path = cont["path"]
 
         unless cont["is_dir"]
           if (cont["revision"] > latest_revision) &&
               (%w{.md .markdown}.include? File.extname(cont_path).downcase)
-            save_to_evernote(cont_path, notebook, cont["revision"])
+            need_update << { dropbox_path: cont_path, notebook: notebook, revision: cont["revision"] }
             updated_revision = cont["revision"] if cont["revision"] > updated_revision
           end
         end
       end
 
       set_revision(path, updated_revision) if updated_revision > latest_revision
+
+      need_update
+    end
+
+    def file_body_of(path)
+      @client.get_file(path)
+    rescue DropboxError => e
+      @logger.error e.message
+      exit 1
     end
 
     def self.auth(app_key, app_secret)
@@ -44,19 +52,6 @@ module Memot
       "memot:#{dir}"
     end
 
-    def save_to_evernote(path, notebook, revision)
-      body = Memot::Markdown.parse_markdown(file_body_of(path))
-      title = File.basename(path)
-
-      if (note_guid = @evernote.get_note_guid(title, notebook)) == ""
-        @evernote.create_note(title, body, notebook)
-        @logger.info "Created: #{notebook}/#{title} (rev. #{revision})"
-      else
-        @evernote.update_note(title, body, notebook, note_guid)
-        @logger.info "Updated: #{notebook}/#{title} (rev. #{revision})"
-      end
-    end
-
     def get_revision(dir)
       key = dir_key_of(dir)
 
@@ -71,13 +66,6 @@ module Memot
     def set_revision(dir, revision)
       key = dir_key_of(dir)
       @redis.set(key, revision)
-    end
-
-    def file_body_of(path)
-      @client.get_file(path)
-    rescue DropboxError => e
-      $stderr.puts e.message
-      exit 1
     end
 
     def file_exists?(dir, name)
